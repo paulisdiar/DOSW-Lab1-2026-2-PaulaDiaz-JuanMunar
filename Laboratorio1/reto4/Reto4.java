de1a125740f809e334ccad2f23e8150bfeec803e
Juan Munar <juan.munar-c@mail.escuelaing.edu.co>
2026-08-08 16:49:26 -0500

    Reto 4 estudiante B con entrada harcodeada

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -2,29 +2,30 @@ package reto4;
 
 import java.util.Hashtable;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Reto4 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Hashtable (ingresar de la forma: plata 8 rubi 4 :");
-        String entrada = sc.nextLine();
+        
+        String entrada = "[(\"plata\",8), (\"rubí\",4), (\"oro\",12), (\"esmeralda\",6)]";
+        System.out.println("Hashtable: " + entrada + "\n");
 
-        //Estudiante B
+        // Estudiante B
         Map<String, String> tabla = crearHashtable(entrada);
         tabla.forEach((clave, valor) -> System.out.println("Clave: " + clave + " | Valor: " + valor));
-        sc.close();
     }
 
     // Estudiante B
     public static Map<String, String> crearHashtable(String entrada) {
         Map<String, String> tabla = new Hashtable<>();
-        String[] partes = entrada.split("\\s+");
+
+        String limpia = entrada.replaceAll("[\\[\\]()\"]", "");
+
+        String[] partes = limpia.split(",\\s*");
 
         for (int i = 0; i < partes.length - 1; i += 2) {
-            String clave = partes[i];
-            String valor = partes[i + 1];
+            String clave = partes[i].trim();
+            String valor = partes[i + 1].trim();
             tabla.put(clave, valor);
         }
 
